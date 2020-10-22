6c662557b7d362f0a0dff67c5451bf7dc062caf2
guueiaw <guueiaw@gmail.com>
2020-10-22 16:58:14 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -9,22 +9,55 @@
           controls
           indicators
           background="#ababab"
+          img-width="1024"
+          img-height="480"
           style="text-shadow: 1px 1px 2px #333"
           @sliding-start="onSlideStart"
           @sliding-end="onSlideEnd"
         >
+          <!-- Text slides with image -->
+          <b-carousel-slide
+            caption="First slide"
+            text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+            img-src="https://picsum.photos/1024/480/?image=52"
+          ></b-carousel-slide>
+
+          <!-- Slides with custom text -->
+          <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+            <h1>Hello world!</h1>
+          </b-carousel-slide>
+
           <!-- Slides with image only -->
           <b-carousel-slide
-            img-src="../assets/imgs/banner.jpg"
+            img-src="https://picsum.photos/1024/480/?image=58"
           ></b-carousel-slide>
-          <!-- Slides with image only -->
+
+          <!-- Slides with img slot -->
+          <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+          <b-carousel-slide>
+            <template #img>
+              <img
+                class="d-block img-fluid w-100"
+                width="1024"
+                height="480"
+                src="https://picsum.photos/1024/480/?image=55"
+                alt="image slot"
+              />
+            </template>
+          </b-carousel-slide>
+
+          <!-- Slide with blank fluid image to maintain slide aspect ratio -->
           <b-carousel-slide
-            img-src="../assets/imgs/banner-1.jpg"
-          ></b-carousel-slide>
-          <!-- Slides with image only -->
-          <b-carousel-slide
-            img-src="../assets/imgs/banner-2.jpg"
-          ></b-carousel-slide>
+            caption="Blank Image"
+            img-blank
+            img-alt="Blank image"
+          >
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse eros felis, tincidunt a tincidunt eget, convallis vel
+              est. Ut pellentesque ut lacus vel interdum.
+            </p>
+          </b-carousel-slide>
         </b-carousel>
       </div>
     </section>
@@ -268,7 +301,7 @@
     <section id="news">
       <div class="container mt-5">
         <div class="row py-5">
-          <h3>ggg</h3>
+          <h3>ข้าวสารจาก ยูนี่ ออนไลน์</h3>
           <div class="col-sm-4 mt-5 mb-3">
             <b-embed type="video" aspect="4by3" controls>
               <source src="../assets/videos/video1.mp4" type="video/mp4" />
