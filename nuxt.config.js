c660c6c0265c11ec3b70294c2afe2fd3dd298e2e
guueiaw <guueiaw@gmail.com>
2020-10-29 17:28:18 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,25 +54,5 @@ export default {
   axios: {},
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-    extend(config) {
-      const vueLoader = config.module.rules.find(
-        (rule) => rule.loader === 'vue-loader'
-      )
-      vueLoader.options.transformAssetUrls = {
-        video: ['src', 'poster'],
-        source: 'src',
-        img: 'src',
-        image: 'xlink:href',
-        'b-avatar': 'src',
-        'b-img': 'src',
-        'b-img-lazy': ['src', 'blank-src'],
-        'b-card': 'img-src',
-        'b-card-img': 'src',
-        'b-card-img-lazy': ['src', 'blank-src'],
-        'b-carousel-slide': 'img-src',
-        'b-embed': 'src',
-      }
-    },
-  },
+  build: {},
 }
