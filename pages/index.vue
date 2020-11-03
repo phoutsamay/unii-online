d9f159f9b1fd3eadf471bd1d4ea99bb58962c7cd
guueiaw <guueiaw@gmail.com>
2020-11-03 14:19:31 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -5,31 +5,59 @@
         <b-carousel
           id="carousel-1"
           v-model="slide"
-          fade
-          :interval="2000"
+          :interval="4000"
           controls
           indicators
           background="#ababab"
           img-width="1024"
           img-height="480"
-          style="text-shadow: 0px 0px 2px #000"
+          style="text-shadow: 1px 1px 2px #333"
           @sliding-start="onSlideStart"
           @sliding-end="onSlideEnd"
         >
-          <!-- Slides with image only -->
+          <!-- Text slides with image -->
           <b-carousel-slide
-            img-src="../assets/imgs/banner.jpg"
+            caption="First slide"
+            text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+            img-src="https://picsum.photos/1024/480/?image=52"
           ></b-carousel-slide>
+
+          <!-- Slides with custom text -->
+          <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+            <h1>Hello world!</h1>
+          </b-carousel-slide>
 
           <!-- Slides with image only -->
           <b-carousel-slide
-            img-src="../assets/imgs/banner-1.jpg"
+            img-src="https://picsum.photos/1024/480/?image=58"
           ></b-carousel-slide>
 
-          <!-- Slides with image only -->
+          <!-- Slides with img slot -->
+          <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+          <b-carousel-slide>
+            <template #img>
+              <img
+                class="d-block img-fluid w-100"
+                width="1024"
+                height="480"
+                src="https://picsum.photos/1024/480/?image=55"
+                alt="image slot"
+              />
+            </template>
+          </b-carousel-slide>
+
+          <!-- Slide with blank fluid image to maintain slide aspect ratio -->
           <b-carousel-slide
-            img-src="../assets/imgs/banner-2.jpg"
-          ></b-carousel-slide>
+            caption="Blank Image"
+            img-blank
+            img-alt="Blank image"
+          >
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse eros felis, tincidunt a tincidunt eget, convallis vel
+              est. Ut pellentesque ut lacus vel interdum.
+            </p>
+          </b-carousel-slide>
         </b-carousel>
       </div>
     </section>
