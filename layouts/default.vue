126f36d524f6a8ece27e4b22b755d415f3d24738
guueiaw <guueiaw@gmail.com>
2020-11-03 17:29:56 +0700

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,43 +1,71 @@
 <template>
   <div>
-    <b-navbar
-      class="sticky-top shadow-sm"
-      toggleable="lg"
-      type="light"
-      variant="white"
-    >
-      <div class="container">
-        <b-navbar-brand href="#"
-          ><img src="../assets/imgs/logo.png" alt=""
-        /></b-navbar-brand>
+    <div class="sticky-top shadow-sm">
+      <b-navbar toggleable="lg" type="light" variant="white">
+        <div class="container">
+          <b-navbar-brand href="#"
+            ><img src="../assets/imgs/logo.png" alt=""
+          /></b-navbar-brand>
 
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+          <!-- <b-navbar-toggle target="nav-collapse"></b-navbar-toggle> -->
 
-        <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav class="mx-auto">
-            <b-nav-item class="px-3" href="#">หน้าแรก</b-nav-item>
-            <b-nav-item class="px-3" href="#">บริการของเรา</b-nav-item>
-            <b-nav-item class="px-3" href="#">เกี่ยวกับเรา</b-nav-item>
-            <b-nav-item class="px-3" href="#">วีดีโอ</b-nav-item>
-            <b-nav-item class="px-3" href="#">ติดต่อเรา</b-nav-item>
-          </b-navbar-nav>
+          <div>
+            <b-navbar-toggle
+              v-b-toggle.sidebar-right
+              target="nav-collapse"
+            ></b-navbar-toggle>
+            <!-- <b-button v-b-toggle.sidebar-right>Toggle Sidebar</b-button> -->
+            <b-sidebar id="sidebar-right" target="nav-collapse" right shadow>
+              <div class="p-3">
+                <nav class="mb-3">
+                  <b-navbar-nav vertical>
+                    <b-nav-item href="#">หน้าแรก</b-nav-item>
+                    <b-nav-item href="#">บริการของเรา</b-nav-item>
+                    <b-nav-item href="#">เกี่ยวกับเรา</b-nav-item>
+                    <b-nav-item href="#">วีดีโอ</b-nav-item>
+                    <b-nav-item href="#">บทความ</b-nav-item>
+                    <b-nav-item href="#">ติดต่อเรา</b-nav-item>
+                  </b-navbar-nav>
+                  <hr />
+                  <b-button
+                    size="sm"
+                    class="btn btn-purple my-2 my-sm-0"
+                    type="submit"
+                    >เข้าสู่ระบบ</b-button
+                  >
+                </nav>
+              </div>
+            </b-sidebar>
+          </div>
 
-          <!-- Right aligned nav items -->
-          <b-navbar-nav class="">
-            <b-nav-form>
-              <b-button
-                size="sm"
-                class="btn btn-purple my-2 my-sm-0"
-                type="submit"
-                >เข้าสู่ระบบ</b-button
-              >
-            </b-nav-form>
-          </b-navbar-nav>
-        </b-collapse>
-      </div>
-    </b-navbar>
+          <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav class="mx-auto topnav">
+              <b-nav-item class="px-3" href="#">หน้าแรก</b-nav-item>
+              <b-nav-item class="px-3" href="#">บริการของเรา</b-nav-item>
+              <b-nav-item class="px-3" href="#">เกี่ยวกับเรา</b-nav-item>
+              <b-nav-item class="px-3" href="#">วีดีโอ</b-nav-item>
+              <b-nav-item class="px-3" href="#">บทความ</b-nav-item>
+              <b-nav-item class="px-3" href="#">ติดต่อเรา</b-nav-item>
+            </b-navbar-nav>
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+              <b-nav-form>
+                <NuxtLink to="navbar">navbar</NuxtLink>
+                <b-button
+                  size="sm"
+                  class="btn btn-purple my-2 my-sm-0"
+                  type="submit"
+                  >เข้าสู่ระบบ</b-button
+                >
+              </b-nav-form>
+            </b-navbar-nav>
+          </b-collapse>
+        </div>
+      </b-navbar>
+    </div>
     <Nuxt />
-    <!-- <section id="footer">
+    <section id="footer">
       <div class="container text-center py-4">
         <img src="../assets/imgs/logo.png" alt="" />
         <p>Connect with us</p>
@@ -52,18 +80,12 @@
         ></a>
         <p class="mt-3">Copyright&copy; 2020 Unii Online</p>
       </div>
-    </section> -->
+    </section>
   </div>
 </template>
 
-<style>
+<style scoped>
 .btn-purple {
-  background: transparent linear-gradient(180deg, #993a96 0%, #69197b 100%) 0%
-    0% no-repeat padding-box;
-  border-radius: 5px;
-  opacity: 1;
-}
-/* .btn-purple {
   background: transparent linear-gradient(180deg, #993a96 0%, #69197b 100%) 0%
     0% no-repeat padding-box;
   border-radius: 5px;
@@ -74,5 +96,5 @@ p {
 }
 i {
   color: #ffffff;
-} */
+}
 </style>
