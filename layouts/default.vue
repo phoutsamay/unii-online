3ca28add9bc4b4274dd7e1fb018c2333079ff6e0
guueiaw <guueiaw@gmail.com>
2020-11-03 15:48:32 +0700

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,35 @@
 <template>
   <div>
-    <b-navbar class="container" toggleable="lg" type="light">
-      <b-navbar-brand href="#"
-        ><img src="../assets/imgs/logo.png" alt=""
-      /></b-navbar-brand>
+    <b-navbar class="strick-top shadow" toggleable="lg" type="light">
+      <div class="container">
+        <b-navbar-brand href="#"
+          ><img src="../assets/imgs/logo.png" alt=""
+        /></b-navbar-brand>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav class="mx-auto">
-          <b-nav-item href="#">หน้าแรก</b-nav-item>
-          <b-nav-item href="#">บริการของเรา</b-nav-item>
-          <b-nav-item href="#">เกี่ยวกับเรา</b-nav-item>
-          <b-nav-item href="#">วีดีโอ</b-nav-item>
-          <b-nav-item href="#">ติดต่อเรา</b-nav-item>
-        </b-navbar-nav>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav class="mx-auto">
+            <b-nav-item href="#">หน้าแรก</b-nav-item>
+            <b-nav-item href="#">บริการของเรา</b-nav-item>
+            <b-nav-item href="#">เกี่ยวกับเรา</b-nav-item>
+            <b-nav-item href="#">วีดีโอ</b-nav-item>
+            <b-nav-item href="#">ติดต่อเรา</b-nav-item>
+          </b-navbar-nav>
 
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="">
-          <b-nav-form>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit"
-              >เข้าสู่ระบบ</b-button
-            >
-          </b-nav-form>
-        </b-navbar-nav>
-      </b-collapse>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="">
+            <b-nav-form>
+              <b-button
+                size="sm"
+                class="btn btn-purple my-2 my-sm-0"
+                type="submit"
+                >เข้าสู่ระบบ</b-button
+              >
+            </b-nav-form>
+          </b-navbar-nav>
+        </b-collapse>
+      </div>
     </b-navbar>
     <Nuxt />
     <!-- <section id="footer">
@@ -47,6 +52,12 @@
 </template>
 
 <style scoped>
+.btn-purple {
+  background: transparent linear-gradient(180deg, #993a96 0%, #69197b 100%) 0%
+    0% no-repeat padding-box;
+  border-radius: 5px;
+  opacity: 1;
+}
 /* .btn-purple {
   background: transparent linear-gradient(180deg, #993a96 0%, #69197b 100%) 0%
     0% no-repeat padding-box;
