b8e45382ed2d2ddc413a91ccc393475d65ab39cf
guueiaw <guueiaw@gmail.com>
2020-11-04 15:39:28 +0700

    update

--- pages/navbar.vue
+++ pages/navbar.vue
@@ -96,6 +96,61 @@
         </p>
       </b-carousel-slide>
     </b-carousel>
+
+    <div class="container mt-5">
+      <b-card style="border: 1px solid #69197b">
+        <div>
+          <h1>เส้นทางวงจรธุรกิจ รีไซเคิลของ Unii</h1>
+          <b-card style="border: none">
+            <img src="../assets/imgs/PTCF.png" alt="" width="100%" />
+          </b-card>
+        </div>
+        <div>
+          <b-card-group deck>
+            <b-card
+              style="border-radius: 5px; box-shadow: 0px 6px 10px #c1c1c133"
+            >
+              <b-card-title>
+                <h3>เป้าหมาย ลดต้นทุน/ลดเวลา/เพิ่มรายได้</h3>
+              </b-card-title>
+              <b-card-text
+                ><ul>
+                  <li>
+                    (ลดต้นทุนกว่า 71%) ให้ผู้รับซื้อทั่วไป (T) มากกว่า 120,000
+                    ล้านบาท/ปี
+                  </li>
+                  <li>
+                    เพิ่มเวลาว่าง (ลดวันทำงานไปกว่า 73%) ให้กับผู้ใช้ Unii
+                    เพื่อทำสิ่งอื่นๆ ได้อย่างมากมาย
+                  </li>
+                  <li>ช่วยรัฐจัดเก็บภาษีได้หลาย 10,000 ล้านบาท/ปี</li>
+                  <li>
+                    สร้างมาตรฐานให้ธุรกิจรีไซเคิลครบทั้งระบบจากต้นน้ำถึงปลายน้ำ
+                  </li>
+                  <li>สร้างมาตรฐานไทย เพื่อ ให้แข่งขันได้ในมาตรฐานโลก</li>
+                </ul></b-card-text
+              >
+            </b-card>
+            <b-card
+              style="border-radius: 5px; box-shadow: 0px 6px 10px #c1c1c133"
+            >
+              <b-card-title>
+                <h3>ลดต้นทุน/ลดเวลา/เพิ่มรายได้"</h3>
+              </b-card-title>
+              <b-card-text
+                ><ul>
+                  <li>ลดต้นทุน 20,000 บาท/เดือน (กว่า 71%)</li>
+                  <li>
+                    ลดเวลาทำงานเหลือ 2 วัน/เที่ยว (กว่า 73%) เพิ่มรายได้ 20,000
+                    บาท/เดือน 240,000 บาท/ปี ได้วันว่าง 22 วัน/เดือน 264 วัน/ปี
+                  </li>
+                </ul></b-card-text
+              >
+            </b-card>
+          </b-card-group>
+        </div>
+      </b-card>
+    </div>
   </div>
 </template>
 
