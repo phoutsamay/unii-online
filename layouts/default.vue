0a812e86d1bad929b007d510292bc899f5760e54
guueiaw <guueiaw@gmail.com>
2020-11-11 15:11:06 +0700

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,12 +19,12 @@
               <div class="p-3">
                 <nav class="mb-3">
                   <b-navbar-nav vertical>
-                    <b-nav-item href="#">หน้าแรก</b-nav-item>
+                    <b-nav-item href="#carousel">หน้าแรก</b-nav-item>
                     <b-nav-item href="#service">บริการของเรา</b-nav-item>
                     <b-nav-item href="#about">เกี่ยวกับเรา</b-nav-item>
                     <b-nav-item href="#video">วีดีโอ</b-nav-item>
                     <b-nav-item href="#blog">บทความ</b-nav-item>
-                    <b-nav-item href="#">ติดต่อเรา</b-nav-item>
+                    <b-nav-item href="#footer">ติดต่อเรา</b-nav-item>
                   </b-navbar-nav>
                   <hr />
                   <b-button
@@ -40,12 +40,12 @@
 
           <b-collapse id="nav-collapse" is-nav>
             <b-navbar-nav class="mx-auto topnav">
-              <b-nav-item class="px-3" href="#">หน้าแรก</b-nav-item>
+              <b-nav-item class="px-3" href="#carousel">หน้าแรก</b-nav-item>
               <b-nav-item class="px-3" href="#service">บริการของเรา</b-nav-item>
               <b-nav-item class="px-3" href="#about">เกี่ยวกับเรา</b-nav-item>
               <b-nav-item class="px-3" href="#video">วีดีโอ</b-nav-item>
               <b-nav-item class="px-3" href="#blog">บทความ</b-nav-item>
-              <b-nav-item class="px-3" href="#">ติดต่อเรา</b-nav-item>
+              <b-nav-item class="px-3" href="#footer">ติดต่อเรา</b-nav-item>
             </b-navbar-nav>
 
             <!-- Right aligned nav items -->
