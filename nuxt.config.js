6fe5f1486b2ec469629b6c0739912858a4377044
guueiaw <guueiaw@gmail.com>
2021-02-23 14:06:55 +0700

    2302211402

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,8 @@
 // const baseURL = 'https://api.unii.co.th'
 const baseURL =
-  process.env.NODE_ENV === 'develop'
+  process.env.NODE_ENV !== 'develop'
     ? 'https://api.unii.co.th'
-    : 'http://localhost:5000'
+    : 'http://127.0.0.1:5000'
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
