14c4cc23bbc875fd5128ecd1f8980be3585c2f69
guueiaw <guueiaw@gmail.com>
2021-03-03 12:11:08 +0700

    0303211202

--- pages/blogs/_id.vue
+++ pages/blogs/_id.vue
@@ -1,20 +1,8 @@
 <template>
   <div class="container">
-    <b-card class="my-5">
-      <b-card-title>{{ post.data.title }}</b-card-title>
-      <small class="text-muted">Last updated 3 mins ago</small>
-      <b-card-body>
-        <b-embed
-          v-if="post.data.video"
-          type="iframe"
-          aspect="16by9"
-          :src="`https://www.youtube.com/embed/${post.data.video}`"
-          allowfullscreen
-        ></b-embed
-      ></b-card-body>
-      <!-- eslint-disable-next-line vue/no-v-html -->
-      <b-card-text v-html="post.data.description"> </b-card-text>
-    </b-card>
+    <p>{{ post }}</p>
+    <h1>{{ post.data.title }}</h1>
+    <p>555</p>
   </div>
 </template>
 
